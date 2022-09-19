80e17a7b672a5c52f972e2d4c582d0783b9ec772
Ratchanont Treevijitpaisan <u7444134@anu.edu.au>
2022-09-19 22:02:02 +0700

    fame local login try

--- app/src/main/java/com/example/marketstructure/LoginActivity.java
+++ app/src/main/java/com/example/marketstructure/LoginActivity.java
@@ -1,14 +1,86 @@
 package com.example.marketstructure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login_email, login_password;
+    private ProgressBar progressBar;
+    private Button login_btn, forget_password;
+    private FirebaseAuth authentic_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_email = (EditText) findViewById(R.id.lg_email);
+        login_password = (EditText) findViewById(R.id.lg_password);
+        login_btn = (Button) findViewById(R.id.lg_loginButton);
+        forget_password = (Button) findViewById(R.id.forget_pass);
+
+        authentic_Login = FirebaseAuth.getInstance();
+
+        if(authentic_Login.getCurrentUser() != null){
+            Intent intent = new Intent(LoginActivity.this, MarketActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = login_email.getText().toString().trim();
+                String password = login_password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Please enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(),"Please enter password!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Toast.makeText(getApplicationContext(),"Password is too short, minimum size is at least 6!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                authentic_Login.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressBar.setVisibility(View.GONE);
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Authentication failed. ", Toast.LENGTH_SHORT).show();
+                        }else{
+                           Intent intent = new Intent(LoginActivity.this, MarketActivity.class);
+                           startActivity(intent);
+                           finish();
+                        }
+                    }
+                });
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/marketstructure/RegisterActivity.java
+++ app/src/main/java/com/example/marketstructure/RegisterActivity.java
@@ -1,14 +1,87 @@
 package com.example.marketstructure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button register_button;
+    private ProgressBar progressBar;
+    private FirebaseAuth authenticator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        authenticator = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        register_button = (Button) findViewById(R.id.sign_up_button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Please enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(),"Please enter password!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Toast.makeText(getApplicationContext(),"Password is too short, minimum size is at least 6!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //Create user
+                authenticator.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(RegisterActivity.this,"create user with email complete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+
+                        if(!task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Success!",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
