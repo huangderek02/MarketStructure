975e72e31a884b6714d6a5dde3d544f3ddd3cfe8
Ratchanont Treevijitpaisan <u7444134@anu.edu.au>
2022-09-19 19:27:39 +0700

    fame local

---
+++ app/src/main/java/com/example/marketstructure/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.marketstructure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.main_login_bt);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton = (Button) findViewById(R.id.main_join_bt);
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
