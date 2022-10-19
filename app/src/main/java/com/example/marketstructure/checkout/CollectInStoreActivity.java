400923ecd757fcd5b08d7e78de6b26552cd171c1
Rhonda Luu <u7127350@anu.edu.au>
2022-10-19 14:38:15 +1100

    created checkout activity UI - still need to update ids for activity_checkout_3_payment_card

---
+++ app/src/main/java/com/example/marketstructure/checkout/CollectInStoreActivity.java
@@ -0,0 +1,16 @@
+package com.example.marketstructure.checkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketstructure.R;
+
+public class CollectInStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_2_collect_in_store);
+    }
+}
---
+++ app/src/main/java/com/example/marketstructure/checkout/PaymentCardActivity.java
@@ -0,0 +1,16 @@
+package com.example.marketstructure.checkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketstructure.R;
+
+public class PaymentCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_3_payment_card);
+    }
+}
